0f2a9e7ee890478623a6b154a02b81bded1c68b9
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-05-17 21:51:18 -0400

    Controller added

---
+++ App/Controllers/Expenses.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+//use \App\Models\Expenses;
+use \App\Models\ExpensesCategoryAssignedToUsers;
+use \App\Flash;
+
+/**
+ * Items controller (example)
+ *
+ * PHP version 7.0
+ */
+class Expenses extends Authenticated
+{
+
+    /**
+     * display new income form
+     *
+     * @return void
+     */
+    public function newAction()
+    {
+        $arr['date_of_expense'] = date('Y-m-d');
+        $arr['amount'] = '0.00';
+        $expense = New \App\Models\Expenses($arr);
+        $categories = ExpensesCategoryAssignedToUsers::
+        getCategoriesAssignedToUser($_SESSION['user_id']);
+
+        View::renderTemplate('Expenses/new.html', [
+            'expenses_categories' => $categories,
+            'expense' => $expense
+        ]);
+    }
+
+    /**
+     * Add a new expense to database
+     *
+     * @return void
+     */
+    public function addAction()
+    {
+        $expense = New \App\Models\Expenses($_POST);
+        if ($expense->add()) {
+
+            Flash::addMessage('Dodano nowy przychód');
+
+            $this->redirect('/Expenses/new');
+
+        } else {
+
+            $categories = ExpensesCategoryAssignedToUsers::
+            getCategoriesAssignedToUser($_SESSION['user_id']);
+
+            View::renderTemplate('Expenses/new.html', [
+                'expenses_categories' => $categories,
+                'expense' => $expense
+            ]);
+        }
+    }
+
+}
