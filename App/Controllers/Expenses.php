e29f2a5fb18d87f2948a51ceb4d91972ce21bfac
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-05-27 21:31:40 -0400

    update balance displayed value using ajax after record remove, join AJAX queries in one function

--- App/Controllers/Expenses.php
+++ App/Controllers/Expenses.php
@@ -67,18 +67,25 @@ class Expenses extends Authenticated
         }
     }
 
-    public function updateTableRowAjax()
-    {
+    private function queries($data) {
         $user_id = $_SESSION['user_id'];
         $date_start = $_POST['date_start'];
         $date_end = $_POST['date_end'];
-        
+
+        $data['incomes_sum'] = \App\Models\Incomes::getIncomesSum($user_id, $date_start, $date_end);
+        $data['expenses_sum'] = \App\Models\Expenses::getExpensesSum($user_id, $date_start, $date_end);
+
+        return $data;
+    }
+
+    public function updateTableRowAjax()
+    {
+        $data = [];
         $expenses = new \App\Models\Expenses($_POST);
         $data['success'] = $expenses->updateTableRowAjax();;
         $data['errors'] = $expenses->errors;
 
-        $data['incomes_sum'] = \App\Models\Incomes::getIncomesSum($user_id, $date_start, $date_end);
-        $data['expenses_sum'] = \App\Models\Expenses::getExpensesSum($user_id, $date_start, $date_end);
+        $data = $this->queries($data);
 
         echo json_encode($data);
         exit;
@@ -87,6 +94,12 @@ class Expenses extends Authenticated
     public function removeTableRowAjax()
     {
         \App\Models\Expenses::removeTableRowAjax();
+
+        $data = [];
+        $data = $this->queries($data);
+
+        echo json_encode($data);
+        exit;
     }
 
 }
--- App/Controllers/Incomes.php
+++ App/Controllers/Incomes.php
@@ -62,18 +62,25 @@ class Incomes extends Authenticated
         }
     }
 
-    public function updateTableRowAjax()
-    {
+    private function queries($data) {
         $user_id = $_SESSION['user_id'];
         $date_start = $_POST['date_start'];
         $date_end = $_POST['date_end'];
 
+        $data['incomes_sum'] = \App\Models\Incomes::getIncomesSum($user_id, $date_start, $date_end);
+        $data['expenses_sum'] = \App\Models\Expenses::getExpensesSum($user_id, $date_start, $date_end);
+
+        return $data;
+    }
+
+    public function updateTableRowAjax()
+    {
+        $data = [];
         $incomes = new \App\Models\Incomes($_POST);
         $data['success'] = $incomes->updateTableRowAjax();
         $data['errors'] = $incomes->errors;
-   
-        $data['incomes_sum'] = \App\Models\Incomes::getIncomesSum($user_id, $date_start, $date_end);
-        $data['expenses_sum'] = \App\Models\Expenses::getExpensesSum($user_id, $date_start, $date_end);
+
+        $data = $this->queries($data);
 
         echo json_encode($data);
         exit;
@@ -82,6 +89,12 @@ class Incomes extends Authenticated
     public function removeTableRowAjax()
     {
         \App\Models\Incomes::removeTableRowAjax();
+
+        $data = [];
+        $data = $this->queries($data);
+
+        echo json_encode($data);
+        exit;
     }
 
 }
