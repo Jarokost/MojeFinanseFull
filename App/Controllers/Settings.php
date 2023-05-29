a8271ca4711e5192d908815bbaae5eb70960c3a8
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-05-28 21:14:09 -0400

    account info update modal adjusted, client and server side validation and update database

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -3,6 +3,9 @@
 namespace App\Controllers;
 
 use \Core\View;
+use \App\Flash;
+use \App\Models\User;
+use \App\Auth;
 
 /**
  * Settings controller
@@ -12,12 +15,58 @@ use \Core\View;
 class Settings extends Authenticated
 {
     /**
+     * user model
+     * 
+     * @var object
+     */
+    public $user;
+
+    /**
+     * Before filter - called before each action method
+     *
+     * @return void
+     */
+    protected function before()
+    {
+        parent::before();
+
+        $this->user = Auth::getUser();
+    }
+
+    /**
      * Balance index
      *
      * @return void
      */
     public function indexAction()
     {
-        View::renderTemplate('Settings/index.html');
+        View::renderTemplate('Settings/index.html', [
+            'user' => $this->user
+        ]);
+    }
+
+    /**
+     * Balance 
+     * 
+     * @return void
+     */
+    public function accountAction()
+    {
+
+        if ($this->user->updateProfile($_POST)) {
+
+            Flash::addMessage('Zapisano zmiany');
+
+            $this->redirect('/settings/index');
+
+        } else {
+
+            Flash::addMessage('Formularz zawiera błędy', 'warning');
+
+            View::renderTemplate('Settings/index.html', [
+                'user' => $this->user
+            ]);
+
+        }
     }
 }
--- App/Models/User.php
+++ App/Models/User.php
@@ -193,25 +193,25 @@ class User extends \Core\Model
 
         // email address
         if (filter_var($this->email, FILTER_VALIDATE_EMAIL) === false) {
-            $this->errors[] = 'Invalid email';
+            $this->errors[] = 'Imię jest wymagane';
         }
         if (static::emailExists($this->email, $this->id ?? null)) {
-            $this->errors[] = 'email already taken';
+            $this->errors[] = 'Podany email jest już zajęty';
         }
 
         // Password
         if (isset($this->password)) {
 
             if (strlen($this->password) < 6) {
-                $this->errors[] = 'Please enter at least 6 characters for the password';
+                $this->errors[] = 'Hasło musi składać się przynajmniej z 6 znaków';
             }
 
             if (preg_match('/.*[a-z]+.*/i', $this->password) == 0) {
-                $this->errors[] = 'Password needs at least one letter';
+                $this->errors[] = 'Hasło musi zawierać przynajmniej jedną literę';
             }
 
             if (preg_match('/.*\d+.*/i', $this->password) == 0) {
-                $this->errors[] = 'Password needs at least one number';
+                $this->errors[] = 'Hasło musi zawierać przynajmniej jedną cyfrę';
             }
         }
     }
