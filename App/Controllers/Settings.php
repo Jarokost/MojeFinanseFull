fd713274681f68feb285234a66f694c18034f931
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-05-30 23:42:08 -0400

    applay to expenses

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -8,7 +8,6 @@ use \App\Auth;
 use \App\Models\IncomesCategoryAssignedToUsers;
 use \App\Models\ExpensesCategoryAssignedToUsers;
 use \App\Models\PaymentMethodsAssignedToUsers;
-use \App\Models\Incomes;
 
 /**
  * Settings controller
@@ -167,9 +166,10 @@ class Settings extends Authenticated
      */
     public function deleteIncomeCategoryAction()
     {
-        $transactions = Incomes::transactionsSumForSelectedCategory($_SESSION['user_id'], $_POST['id']);
+        $transactions = IncomesCategoryAssignedToUsers::transactionsSumForSelectedCategory($_SESSION['user_id'], $_POST['id']);
         $category_name = IncomesCategoryAssignedToUsers::getCategoryName($_POST['id']);
         $force = $_POST['force'];
+
         if($transactions && $force === 'n') {
 
             $data['flash_message_body'][0] = 'dla podanej kategorii: "' . $category_name['name'] . '" istnieją (' . $transactions['transactions'] . ') transakcje, niepowodzenie';
@@ -258,11 +258,26 @@ class Settings extends Authenticated
      */
     public function deleteExpenseCategoryAction()
     {
+        $transactions = ExpensesCategoryAssignedToUsers::transactionsSumForSelectedCategory($_SESSION['user_id'], $_POST['id']);
         $category_name = ExpensesCategoryAssignedToUsers::getCategoryName($_POST['id']);
-        ExpensesCategoryAssignedToUsers::removeCategory($_POST['id']);
+        $force = $_POST['force'];
 
-        $data['flash_message_body'][0] = 'usunięto kategorię: ' . $category_name['name'];
-        $data['flash_message_type'][0] = 'info';
+        if($transactions && $force === 'n') {
+
+            $data['flash_message_body'][0] = 'dla podanej kategorii: "' . $category_name['name'] . '" istnieją (' . $transactions['transactions'] . ') transakcje, niepowodzenie';
+            $data['flash_message_type'][0] = 'warning';
+
+            $data['category_name'] = $transactions['category_name'];
+            $data['transactions'] = $transactions['transactions'];
+
+        } else {
+
+            //ExpensesCategoryAssignedToUsers::removeCategory($_POST['id']);
+
+            $data['flash_message_body'][0] = 'usunięto kategorię: ' . $category_name['name'];
+            $data['flash_message_type'][0] = 'info';
+
+        }
 
         $data['categories'] = ExpensesCategoryAssignedToUsers::
         getCategoriesAssignedToUser($_SESSION['user_id']);
@@ -335,11 +350,26 @@ class Settings extends Authenticated
      */
     public function deletePaymentMethodAction()
     {
+        $transactions = PaymentMethodsAssignedToUsers::transactionsSumForSelectedCategory($_SESSION['user_id'], $_POST['id']);
         $category_name = PaymentMethodsAssignedToUsers::getCategoryName($_POST['id']);
-        PaymentMethodsAssignedToUsers::removeCategory($_POST['id']);
+        $force = $_POST['force'];
 
-        $data['flash_message_body'][0] = 'usunięto kategorię: ' . $category_name['name'];
-        $data['flash_message_type'][0] = 'info';
+        if($transactions && $force === 'n') {
+
+            $data['flash_message_body'][0] = 'dla podanej kategorii: "' . $category_name['name'] . '" istnieją (' . $transactions['transactions'] . ') transakcje, niepowodzenie';
+            $data['flash_message_type'][0] = 'warning';
+
+            $data['category_name'] = $transactions['category_name'];
+            $data['transactions'] = $transactions['transactions'];
+
+        } else {
+
+            //PaymentMethodsAssignedToUsers::removeCategory($_POST['id']);
+
+            $data['flash_message_body'][0] = 'usunięto kategorię: ' . $category_name['name'];
+            $data['flash_message_type'][0] = 'info';
+
+        }
 
         $data['categories'] = PaymentMethodsAssignedToUsers::
         getCategoriesAssignedToUser($_SESSION['user_id']);
