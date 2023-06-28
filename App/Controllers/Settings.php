e8f5fa93c8cf6e10d2d628822e3f286285c8e394
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-06-27 21:44:50 -0400

    settings page PHP - updateExpenseCategoryAction changed message

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -447,9 +447,15 @@ class Settings extends Authenticated
             empty($post_fetch_promise['name']) ? NULL : $post_fetch_promise['name'],
             $post_fetch_promise['limit_value'] ?? NULL);
 
-            $data['flash_message_body'][0] = 'Zmieniono nazwę kategorii z: ' . $category_name['name'] . ' na: ' . $post_fetch_promise['name'];
-            if ( isset($post_fetch_promise['limit_value']) ) {
-                $data['flash_message_body'][0] .= ' oraz dodano limit: ' + $post_fetch_promise['limit_value'];
+            if (!empty($post_fetch_promise['name']) && isset($post_fetch_promise['limit_value'])) {
+                $data['flash_message_body'][0] = 'Zmieniono nazwę kategorii z: ' . $category_name['name'] . ' na: ' . $post_fetch_promise['name'];
+                $data['flash_message_body'][0] .= ' oraz dodano limit: ' . $post_fetch_promise['limit_value'];
+            } else if (empty($post_fetch_promise['name']) && isset($post_fetch_promise['limit_value'])) {
+                $data['flash_message_body'][0] = 'Zmieniono limit dla kategorii: ' . $category_name['name'] . ' na: ' . $post_fetch_promise['limit_value'];
+            } else if (!empty($post_fetch_promise['name']) && !isset($post_fetch_promise['limit_value'])) {
+                $data['flash_message_body'][0] = 'Zmieniono nazwę kategorii z: ' . $category_name['name'] . ' na: ' . $post_fetch_promise['name'];
+            } else {
+                $data['flash_message_body'][0] = 'Bez zmian';
             }
             $data['flash_message_type'][0] = 'info';
 
